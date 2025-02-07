196982eba3fa22ade86ad0a1851563de41b95220
Alistair Brown <github@alistairjcbrown.com>
2025-02-07 14:55:03 +0000

    Init

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+
+(async function () {
+  const [node, script, ...parameters] = process.argv;
+  console.log(`Node: ${node}`);
+  console.log(`Script: ${script}`);
+  console.log(`Parameters: ${parameters.map((arg) => `"${arg}"`).join(", ")}`);
+})();
